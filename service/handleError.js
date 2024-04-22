e119f80d474549ff43e31e5f46e5126e22e7f4eb
runweiting <whatisfuntime@gmail.com>
2024-04-22 22:41:47 +0800

    feat: 🎸 修改 handleError 加入 res.headersSent 判斷

--- service/handleError.js
+++ service/handleError.js
@@ -1,13 +1,16 @@
 const headers = require("./corsHeader");
 function handleError(res, error) {
-  res.writeHead(400, headers);
-  res.write(
-    JSON.stringify({
-      status: "false",
-      message: error,
-    })
-  );
-  res.end();
+  // headersSent 是 http.ServerResponse 的一個屬性為布林值，用於檢查 res 的 headers 是否已發送，已發送 true，否為 false，在 Express 中，res 是 http.ServerResponse 的擴展，因此也具有 headersSent 屬性
+  if (!res.headersSent) {
+    res.writeHead(400, headers);
+    res.write(
+      JSON.stringify({
+        status: "false",
+        message: error || "An error occurred",
+      })
+    );
+    res.end();
+  }
 }
 
 module.exports = handleError;
